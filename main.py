14ddeb06e372beb719ec5c323982d3a1ad22c2cb
ece-mohammad <kuro.ece@gmail.com>
2019-03-25 20:00:36 +0200

    - Added always on top check button

--- main.py
+++ main.py
@@ -14,21 +14,26 @@ def main():
     root = tk.Tk()
     root.title("Resiter Edit Tool")     # window title
     root.resizable(0, 0)                # make it unresizable
-    root.attributes("-topmost", True)   # make it always on top
+    # root.attributes("-topmost", True)   # make it always on top
 
     # initlize the main frame
     main_frame = ttk.Frame(root, padding="3 3 12 12", relief="sunken", borderwidth=3)
     main_frame.grid(row=0, column=0, sticky=tk.NSEW)
 
+    # add a checkbutton for always on top
+    on_top_var = tk.BooleanVar(0)
+    a_on_top = tk.Checkbutton(main_frame, text="Always on top", variable=on_top_var, command=lambda: root.attributes("-topmost", on_top_var.get()))
+    a_on_top.grid(row=0, column=0, sticky=(tk.W,))
+
     # add register frame and initilize it
     reg_frame = RegisterFrame(main_frame)
-    reg_frame.grid(column=0, row=0)
+    reg_frame.grid(row=1, column=0)
     reg_frame.init_frame()
 
     # add confirm frame and initilize it
     confirm_frame = ConfirmFrame(main_frame)
     confirm_frame.init_frame()
-    confirm_frame.grid(row=1, column=0)
+    confirm_frame.grid(row=2, column=0)
 
     # start main event loop
     root.mainloop()
